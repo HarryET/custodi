52c18fec2d63f071a58b300125d163e3436d5799
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-12-07 11:45:43 -0500

    Update login and register pages
    Co-authored-by: Victor <peralta.r.victor@gmail.com>
    Co-authored-by: Harry Bairstow <h.bairstow22@gmail.com>

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -5,12 +5,12 @@ import { useEffect } from 'react'
 import { useAuth } from '../../hooks/useAuth'
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  const { session, user } = useAuth();
+  const router = useRouter()
+  const { session } = useAuth()
 
   useEffect(() => {
-    if(session == null) {
-      router.push("/login")
+    if (session == null) {
+      router.push('/login')
     }
   })
 
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <title>Custodi</title>
       </Head>
       <div>
-          <p>This page will have all your organizations and their projects.</p>
+        <p>This page will have all your organizations and their projects.</p>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: '#7C62E3',
+        secondary: '#674ADA',
+        error: "#D33F49",
+        warn: "#FEC601",
+        success: "#0CCA4A",
+        discord: "#5865f2",
+        twitter: "#1da1f2",
         "supabase": "#24B47E"
-      }
+      },
+      dropShadow: {
+         '3xl': '0 3px 4px rgba(0, 0, 0, 0.25)'
+       }
     },
   },
   variants: {

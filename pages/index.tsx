d4df8fc4f7bc1c84180375589393a31148f9a2f3
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2021-12-07 18:54:56 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Home: NextPage = () => {
@@ -8,40 +9,25 @@ const Home: NextPage = () => {
       <Head>
         <title>Custodi</title>
       </Head>
-      <div className="w-full h-full flex flex-col items-center justify-start">
-        <div className="w-full flex flex-row items-center justify-end space-x-4">
-          {/* Header */}
-          <Link href="/docs">
-            <p className="hover:text-secondary hover:underline cursor-pointer">Docs</p>
-          </Link>
-          <Link href="https://supabase.com/blog/2021/12/03/supabase-holiday-hackdays-hackathon">
-            <p className="hover:text-supabase hover:underline cursor-pointer">Supabase Hackathon</p>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center justify-center w-full h-auto pt-6 md:h-full md:flex-row">
-          <div className="md:w-1/2">
-            <div>
-              {/* Title & Description */}
-              <h1 className="text-6xl font-bold text-center">Custodi</h1>
-              <p className="text-lg text-center">The open source Sentry alternative.</p>
-            </div>
-            <div className="my-6 w-full flex flex-col items-center justify-center">
-              <div className="px-8 py-2 bg-primary hover:bg-secondary text-white rounded">
-                <Link href="/register">
-                  Get Started
-                </Link>
-              </div>
-              <div>
-                <p>or <span className="text-primary hover:text-secondary hover:underline"><Link href="/login">Login</Link></span></p>
-              </div>
+      <section className="text-gray-800 body-font mx-8">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">Custodi</h1>
+            <p className="text-gray-600 mb-8 leading-relaxed">The open source Sentry alternative.</p>
+            <div className="flex justify-center">
+              <Link href='/login'>
+                <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg">Get Started</button>
+              </Link>
+              <Link href='/doc'>
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Documentation</button>
+              </Link>
             </div>
           </div>
-          <div className="mb-6 mx-6 md:h-1/6 md:w-auto md:m-12">
-            {/* Video/Image */}
-            <img src={"/assets/product-demo-img.jpg"} alt={"Placeholder Photo by Katarzyna Modrzejewska"} />
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img className="object-cover object-center rounded" alt="hero" src="/assets/product-demo-img.jpg" />
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }

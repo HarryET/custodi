9ed490e5949e90808263bb851c6716cc975682c4
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2021-12-08 20:27:25 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="text-gray-800 pt-4 px-10 flex justify-between">
       <h1 className="text-xl font-bold">Custodi</h1>
-      <div className="">
+      <div>
         <Link href="/">
           <a className="hover:text-primary mr-3">Docs</a>
         </Link>

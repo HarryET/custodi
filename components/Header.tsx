f4d738c43bee9d531024e6fad3d6713a3bd31d58
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2021-12-08 20:17:46 -0500

    fix scroll bug

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className="text-gray-800 pt-4 px-10 flex justify-between">
+      <h1 className="text-xl font-bold">Custodi</h1>
+      <div className="">
+        <Link href="/">
+          <a className="hover:text-primary mr-3">Docs</a>
+        </Link>
+        <a
+          href="https://supabase.com/blog/2021/12/03/supabase-holiday-hackdays-hackathon"
+          className="hover:text-supabase ml-3"
+        >
+          Supabase Hackathon
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,22 +9,35 @@ const Home: NextPage = () => {
       <Head>
         <title>Custodi</title>
       </Head>
-      <section className="text-gray-800 body-font mx-8">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <Header />
+      <section className="w-full mt-20 sm:flex sm:flex-col sm:justify-center sm:items-center text-gray-800">
+        <div className="px-10 container mx-auto flex md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">Custodi</h1>
-            <p className="text-gray-600 mb-8 leading-relaxed">The open source Sentry alternative.</p>
+            <h1 className="title-font sm:text-4xl md:text-5xl text-3xl mb-4 font-bold text-gray-800">
+              Custodi
+            </h1>
+            <p className="text-gray-600 mb-8 leading-relaxed">
+              The open source Sentry alternative.
+            </p>
             <div className="flex justify-center">
-              <Link href='/login'>
-                <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg">Get Started</button>
+              <Link href="/login" passHref>
+                <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg">
+                  Get Started
+                </button>
               </Link>
-              <Link href='/doc'>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Documentation</button>
+              <Link href="/doc" passHref>
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  Documentation
+                </button>
               </Link>
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded" alt="hero" src="/assets/product-demo-img.jpg" />
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 grid justify-items-center">
+            <img
+              className="object-cover object-center rounded h-imgSM"
+              alt="hero"
+              src="/assets/product-demo-img.jpg"
+            />
           </div>
         </div>
       </section>

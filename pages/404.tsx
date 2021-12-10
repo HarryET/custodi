dbe162c3d7707391ca1dd36322b0c51e2660eca7
Aiden Schrock <68505349+aidenschrock@users.noreply.github.com>
2021-12-09 19:20:05 -0600

    Update 404.tsx
    Updated font and image size

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,10 +14,10 @@ const Register: NextPage = () => {
       <div className="w-full mt-10 sm:flex sm:flex-col sm:justify-center sm:items-center text-gray-800">
         <div className="h-full flex flex-col items-center justify-center mx-6 sm:w-1/2 lg:w-2/6">
           <div className="w-full flex flex-col items-center justify-center">
-            <h1 className="text-7xl font-bold text-center mt-6 sm:mb-1">404</h1>
-            <p className="mb-3 font-bold">Uh Oh! There&apos;s nothing here!</p>
-            <Image src="/assets/404.svg" width={400} height={307} alt="404" />
-            <div className="flex mt-3">
+            <h1 className="text-8xl font-bold text-center mt-6 sm:mb-1">404</h1>
+            <p className="text-2xl mb-3 font-bold">Uh Oh! There&apos;s nothing here!</p>
+            <Image src="/assets/404.svg" width={675} height={431} alt="404" />
+            <div className="flex text-2xl mt-3">
               <p className="font-bold">Go back</p>
               <Link href="/">
                 <a className="text-primary hover:text-secondary ml-1 font-bold">Home</a>

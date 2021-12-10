d0653da0a835484b3ed20b984dcb2275e0bbef9f
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2021-12-09 19:38:49 -0500

    fix: landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Header from '../components/Header'
+import ProductDemoImg from '../public/assets/product-demo-img'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
         <title>Custodi</title>
       </Head>
       <Header />
-      <section className="w-full mt-20 sm:flex sm:flex-col sm:justify-center sm:items-center text-gray-800">
+      <section className="w-full mt-20 mb-10 sm:flex sm:flex-col sm:justify-center sm:items-center text-gray-800">
         <div className="px-10 container mx-auto flex md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl md:text-5xl text-3xl mb-4 font-bold text-gray-800">
@@ -32,12 +33,8 @@ const Home: NextPage = () => {
               </Link>
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 grid justify-items-center">
-            <img
-              className="object-cover object-center rounded h-imgSM"
-              alt="hero"
-              src="/assets/product-demo-img.jpg"
-            />
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full grid justify-items-center">
+            <ProductDemoImg />
           </div>
         </div>
       </section>
